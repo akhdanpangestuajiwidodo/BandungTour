410e1efe6a87fb5d5f492f7397ed2ac8cfbaca26
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-07-25 14:06:30 +0700

    Create column layout structure and make the title content

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
 
       ),
+      home: DetailScreen(),
     );
   }
+}
+
+class DetailScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 16.0) ,
+              child: Text(
+                "Farm House Lembang",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
 }
 
