805859bc7ab42f9f3ccd2fecf8430c30e4296a52
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-07-25 14:22:58 +0700

    Create description text of place in 3rd child of column widget

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class DetailScreen extends StatelessWidget{
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
+          children: <Widget>[
             Container(
               margin: EdgeInsets.only(top: 16.0) ,
               child: Text(
@@ -40,9 +40,42 @@ class DetailScreen extends StatelessWidget{
             Container(
               margin: EdgeInsets.symmetric(vertical: 16.0),
               child: Row(
-                children: [
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
 
+                    Column(
+                      children: <Widget>[
+                        Icon(Icons.calendar_today),
+                        SizedBox(height: 8.0),
+                        Text("Open Everiday"),
+                      ],
+                    ),
+
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.lock_clock),
+                      SizedBox(height: 8.0),
+                      Text("09.00 - 20.00"),
+                    ],
+                  ),
+
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.price_change),
+                      SizedBox(height: 8.0),
+                      Text("Rp. 25.000"),
+                    ],
+                  ),
                 ],
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16.0),
               ),
             ),
           ],
